d7c686dd846df52d14bba639f01da9a2d3272e1d
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-01-21 16:38:50 +0530

    Rename naveen.c to naveenkumar.c

---
+++ naveenkumar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,N;
+printf("Enter the value of a:");
+scanf("%d",&a);
+printf("Enter the value of b:");
+sacnf("%d",&b);
+if(N%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("Odd");
+}
